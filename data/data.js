8ad5de871dcc7779da81d74ecd4f45a4ef6a1589
Arjun <arjun.drag007@gmail.com>
2024-09-05 01:46:50 +0530

    initial commit

---
+++ data/data.js
@@ -0,0 +1,112 @@
+export const data = [
+  {
+    id: 1,
+    name: {
+      first: 'Hemant',
+      last: 'Agnihotri',
+    },
+    total: 223676,
+    status: 'On Hold',
+    method: 'Paytm',
+    date: '15 Minutes ago',
+  },
+  {
+    id: 2,
+    name: {
+      first: 'Himanshu',
+      last: 'Sharma',
+    },
+    total: 95676,
+    status: 'Processing',
+    method: 'PhonePe',
+    date: '23 Minutes ago',
+  },
+  {
+    id: 3,
+    name: {
+      first: 'Vaishali',
+      last: 'Sharma',
+    },
+    total: 39600,
+    status: 'Completed',
+    method: 'Visa',
+    date: '1 Hour ago',
+  },
+  {
+    id: 4,
+    name: {
+      first: 'Naman',
+      last: 'Yadav',
+    },
+    total: 12000,
+    status: 'Processing',
+    method: 'MasterCard',
+    date: '1 Hour ago',
+  },
+  {
+    id: 5,
+    name: {
+      first: 'Ashutosh',
+      last: 'Kumar',
+    },
+    total: 14000,
+    status: 'On Hold',
+    method: 'Gpay',
+    date: '2 Hour ago',
+  },
+  {
+    id: 6,
+    name: {
+      first: 'Shruti',
+      last: 'Jha',
+    },
+    total: 103600,
+    status: 'Completed',
+    method: 'Check',
+    date: '3 Hour ago',
+  },
+  {
+    id: 7,
+    name: {
+      first: 'Hariom',
+      last: 'Sharan',
+    },
+    total: 7100,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '3 Hour ago',
+  },
+  {
+    id: 8,
+    name: {
+      first: 'Pawan',
+      last: 'Singh',
+    },
+    total: 55120,
+    status: 'Completed',
+    method: 'Visa',
+    date: '7 Hour ago',
+  },
+  {
+    id: 9,
+    name: {
+      first: 'Shivam',
+      last: 'Singh',
+    },
+    total: 1120,
+    status: 'Completed',
+    method: 'Paytm',
+    date: '1 Day ago',
+  },
+  {
+    id: 10,
+    name: {
+      first: 'Sonu',
+      last: 'Yadav',
+    },
+    total: 17440,
+    status: 'Completed',
+    method: 'MasterCard',
+    date: '1 Day ago',
+  },
+];
